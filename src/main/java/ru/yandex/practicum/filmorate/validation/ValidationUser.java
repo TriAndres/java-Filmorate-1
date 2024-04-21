81a6d578fa50734c17b98a13b65e09dd40470407
Andrei <An.S-1@yandex.ru>
2024-04-21 19:20:29 +0300

    4

--- src/main/java/ru/yandex/practicum/filmorate/validation/ValidationUser.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ValidationUser.java
@@ -4,17 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.exCeption.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 @Slf4j
 public class ValidationUser {
-    /*
-    Для User:
-дата рождения не может быть в будущем.
-     */
+
     public void validation(User user) {
         String[] lineEmail = user.getEmail().split("");
         String email = "-1";
@@ -42,40 +37,16 @@ public class ValidationUser {
 
         if (user.getName().isEmpty()) {
             user.setName(user.getLogin());
-           log.info("имя для отображения может быть пустым — в таком случае будет использован логин");
+            log.info("имя для отображения может быть пустым — в таком случае будет использован логин");
         }
 
-        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate nowDate = LocalDate.now();
+        LocalDate birthday = LocalDate.parse(user.getBirthday(), formatter);
 
-    }
-
-
-    public static void main(String[] args) {
-
-        //"2024/12/12"
-        LocalDate date = LocalDate.now();
-        String text = date.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-        LocalDate parsedDate = LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-        System.out.println(parsedDate.isAfter(date.minusDays(1)));
-
-        String date1 = "01.03.2016";
-        String date2 = "01.02.2016";
-
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-
-        Date dateOne = null;
-        Date dateTwo = null;
-
-        try {
-            dateOne = format.parse(date1);
-            dateTwo = format.parse(date2);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (nowDate.isBefore(birthday)) {
+            log.info("дата рождения не может быть в будущем");
+            throw new ValidationException("дата рождения не может быть в будущем");
         }
-
-        assert dateOne != null;
-        System.out.println(dateOne.before(dateTwo));
-        System.out.println(dateTwo);
     }
-
 }
